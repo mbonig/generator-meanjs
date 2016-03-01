f8c5e9b0a9f75355ca7f5e03c38db743c2b742e1
Matthew Bonig <matthew.bonig@gmail.com>
2016-02-29 20:20:19 -0700

    adding example menu to config

--- angular-config/templates/_.client.config.js
+++ angular-config/templates/_.client.config.js
@@ -1,14 +1,20 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  // <%= humanizedModuleName %> module config
-  angular
-    .module('<%= slugifiedModuleName %>')
-    .run(menuConfig);
+    // <%= humanizedModuleName %> module config
+    angular
+        .module('<%= slugifiedModuleName %>')
+        .run(menuConfig);
 
-  menuConfig.$inject = ['Menus'];
+    menuConfig.$inject = ['Menus'];
 
-  function menuConfig(Menus) {
+    function menuConfig(Menus) {
+        Menus.addMenuItem('topbar', {
+            title: '<%= slugifiedModuleName %>',
+            state: '<%= slugifiedModuleName %>.list',
+            roles: ['*'],
+            position: 0
+        });
     }
 
 })();
